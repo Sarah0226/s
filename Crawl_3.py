98798202fbfced3ef1489963fefad87a78d68659
Sarah0226 <81091945+Sarah0226@users.noreply.github.com>
2021-03-25 18:08:23 +0800

    Update Crawl_3.py

--- Crawl_3.py
+++ Crawl_3.py
@@ -1,13 +1,12 @@
 # -*- coding: UTF-8 -*-
-##到蝦皮首頁搜尋iphone x，並抓前300個商品的銷售量以及shopid
 import io
 import logging
 import time
 import requests
+import json
 from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 ##Create a file and setting its format
 log_format = "%(asctime)s %(levelname)s Line %(lineno)d: %(message)s"
@@ -25,30 +24,43 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
+logging.info('Set a dictionary to store info')
 seller_sales = {}
+
+##Open file sales_amount and set as write mode then put into 'Sellers_Sales_volume'
+logging.info('Open file sales_amount and set as write mode then put into : Sellers_Sales_volume')
+Sellers_Sales_volume = io.open("sales_amount.txt","w",encoding='utf-8')
 
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
     'X-Requested-With': 'XMLHttpRequest'
 }    
+logging.info('Download the webdriver first, and enter the store path')
 driver = webdriver.Chrome(executable_path=r"C:\Users\sarah.chao\Documents\Python\chromedriver_win32\chromedriver.exe")
 
-time.sleep(1)
-
-#q = driver.find_element_by_class_name('shopee-searchbar-input__input')
+#time.sleep(1)
+logging.info('Let user enter the product keyword')
 print("Please fill the name you want to search")
-item = str(input())
-#page = 0
+logging.info('Get the keyword')
+item = input()
+
+logging.info('Create two lists : shop_id and sale_vol to store follow-up processing data')
 shop_id=[]
 sale_vol=[]
+logging.info('Make the window Full screen')
+driver.maximize_window()
 
-
-##抓300個商品，一頁50個要6頁
+##Crawl 300 items, a page has 50 items so have to turn 6 pages'
+logging.info('Crawl 300 items, a page has 50 items so have to turn 6 pages')
 for page in range(6): 
-    url = "https://shopee.tw/search?keyword="+item+"&page="+ str(page)
-    driver.maximize_window()
+
+    logging.info('Set the url, keyword, page')
+    url = "https://shopee.tw/search?keyword="+str(item)+"&page="+str(page)
+    
+    logging.info('Driver get the url and pause action  (sleep) sleep to catch info')
     driver.get(url)
     time.sleep(3)
+    logging.info('Let the window scroll down till the lowest section and pause action (sleep) to catch info')
     driver.execute_script("window.scrollTo(0, 500)")
     time.sleep(3)
     driver.execute_script("window.scrollTo(0, 1000)")
@@ -64,39 +76,69 @@ for page in range(6):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     time.sleep(3)
 
+    logging.info('Load the soup')
     soup = BeautifulSoup(driver.page_source,'lxml')
 
+    logging.info('In order to crawl the info of html, make soup to find the class_ and capture sales and items')
     sales = soup.find_all("div", class_= "go5yPW")
     items = soup.find_all("div", class_="col-xs-2-4 shopee-search-item-result__item")
 
-
+    logging.info('Capture the str within href and get the shop_id in Second to last')
     for shop in items:
         shops = shop.find("a")["href"]
         shop_id.append(shops.split(".")[-2])
-        #print(shop_id)
- 
+
+    logging.info('Capture the str within sales and only get the sale_vol number, so store the second index. Besides, some items have not sales volume, need to give it 0')
     for sale in sales:
         if sale.text:
             sale_vol.append(sale.text.split(" ")[1])
         else:
-            sale_vol.append(0)
+            sale_vol.append('0')
 
-        #sale_vol = sale_vol.replace(",","")
-        sale_vol=int(sale_vol)
-        #print(sale_vol)
-    
-    seller_sales = dict(zip(shop_id,sale_vol))
-    # = int(sale_vol)
-    print(shop_id)
-    print (len(shop_id))
-    print(sale_vol)
-    print (len(sale_vol))
-    print(seller_sales)
-    page =+ 1
+    logging.info('Turn page')
+    page += 1
 
+logging.info('Once the capture is over, the browser closes')
+driver.close()
 
-##Open file sales_amount and set as write mode then put into 'sale'
-sale = io.open("sales_amount.txt","w",encoding='utf-8')
+logging.info('Calculate sale_vol, if appear "," then remove, and if appear "萬" then convert to float and *10000. Last, all sale_vol convert to int.')
+for num in range(len(sale_vol)):
+    ##remove the ','
+    if "," in sale_vol[num]:
+        sale_vol[num] = sale_vol[num].replace(",", "")
 
+    ## convert the '萬'
+    if "萬" in sale_vol[num]:
+        number = float(sale_vol[num].replace("萬", "")) * 10000
+        sale_vol[num] = number
 
-#driver.close()
+    ## convert to int
+    sale_vol[num] = int(sale_vol[num])
+
+logging.info('Create a dictionary: seller_dict')
+seller_dict = {}
+logging.info('Put shop_id,sale_vol into seller_dict')
+for key,value in zip(shop_id,sale_vol):
+   seller_dict[key] = seller_dict.get(key,0)+value
+
+logging.info('Create a dictionary: sorted_seller, ')
+sorted_seller = {}
+sorted_seller_2 = sorted(seller_dict, key=seller_dict.get,reverse=True)
+
+logging.info('Get first 50 number , if less than 50 then get current quantity')
+for seq in sorted_seller_2:
+   if len(sorted_seller) < 50:
+      sorted_seller[seq] = seller_dict[seq]
+   else:
+      break
+
+logging.info('Create a list: store_file to temporary storage and set queue = 1')
+store_file = []
+queue = 1
+
+logging.info('Finally, modify the foramt.')
+for target in sorted_seller:
+   print("The number", queue ,"shop is", target ,"with sales amount" ,sorted_seller[target])
+   store_file = ("The number", queue ,"shop is", target ,"with sales amount", sorted_seller[target])
+   Sellers_Sales_volume.write(str(store_file)+'\n') 
+   queue += 1
